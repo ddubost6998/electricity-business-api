9d145716678220d80aa8bb250589762c7e404326
Damien <ddubost6998@gmail.com>
2024-10-25 15:18:41 +0200

    Initial commit

---
+++ src/test/java/fr/humanbooster/electricity_business/ElectricityBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.humanbooster.electricity_business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricityBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
