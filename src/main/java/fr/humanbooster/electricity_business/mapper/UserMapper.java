65a4e4848448d273b8f5e2a820e51e7224bfc7a3
Damien <ddubost6998@gmail.com>
2024-11-22 11:06:39 +0100

    Add UserMapper

---
+++ src/main/java/fr/humanbooster/electricity_business/mapper/UserMapper.java
@@ -0,0 +1,37 @@
+package fr.humanbooster.electricity_business.mapper;
+
+import org.springframework.stereotype.Component;
+import fr.humanbooster.electricity_business.dto.UserDTO;
+import fr.humanbooster.electricity_business.model.User;
+
+@Component
+public class UserMapper {
+
+    public UserDTO toDTO(User entity) {
+        if (entity == null) {
+            return null;
+        }
+
+        UserDTO dto = new UserDTO();
+        dto.setId(entity.getId());
+        dto.setFirstname(entity.getFirstname());
+        dto.setLastname(entity.getLastname());
+        dto.setEmail(entity.getEmail());
+        dto.setPhone(entity.getPhone());
+        return dto;
+    }
+
+    public User toEntity(UserDTO dto) {
+        if (dto == null) {
+            return null;
+        }
+
+        User entity = new User();
+        entity.setId(dto.getId());
+        entity.setFirstname(dto.getFirstname());
+        entity.setLastname(dto.getLastname());
+        entity.setEmail(dto.getEmail());
+        entity.setPhone(dto.getPhone());
+        return entity;
+    }
+}
