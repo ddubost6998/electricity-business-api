bace5c29c626e75bbb50fd03d619ffbcbb9f007d
Damien <ddubost6998@gmail.com>
2024-11-22 11:09:39 +0100

    Add LocationMapper

---
+++ src/main/java/fr/humanbooster/electricity_business/mapper/LocationMapper.java
@@ -0,0 +1,43 @@
+package fr.humanbooster.electricity_business.mapper;
+
+import org.springframework.stereotype.Component;
+
+import fr.humanbooster.electricity_business.dto.LocationDTO;
+import fr.humanbooster.electricity_business.model.Location;
+
+@Component
+public class LocationMapper {
+
+    public LocationDTO toDTO(Location entity) {
+        if (entity == null) {
+            return null;
+        }
+
+        return new LocationDTO(
+            entity.getId(),
+            entity.getAddress(),
+            entity.getCity(),
+            entity.getZipcode(),
+            entity.getLatitude(),
+            entity.getLongitude(),
+            entity.getCountry()
+        );
+    }
+
+    public Location toEntity(LocationDTO dto) {
+        if (dto == null) {
+            return null;
+        }
+
+        Location location = new Location();
+        location.setId(dto.getId());
+        location.setAddress(dto.getAddress());
+        location.setCity(dto.getCity());
+        location.setZipcode(dto.getZipcode());
+        location.setLatitude(dto.getLatitude());
+        location.setLongitude(dto.getLongitude());
+        location.setCountry(dto.getCountry());
+
+        return location;
+    }
+}
