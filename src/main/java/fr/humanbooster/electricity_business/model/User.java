af210b88c0de93ed54de2dbaeed72b2accf4399b
Damien <ddubost6998@gmail.com>
2025-07-03 13:34:45 +0200

    Add equals & hashCode for User

--- src/main/java/fr/humanbooster/electricity_business/model/User.java
+++ src/main/java/fr/humanbooster/electricity_business/model/User.java
@@ -1,102 +1,14 @@
 package fr.humanbooster.electricity_business.model;
 
 import jakarta.persistence.*;
-import org.springframework.security.crypto.bcrypt.BCrypt;
-
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "app_user")
 public class User {
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public void setBirthdate(LocalDate birthdate) {
-        this.birthdate = birthdate;
-    }
-
-    public void setVerificationCode(String verificationCode) {
-        this.verificationCode = verificationCode;
-    }
-
-    public void setVerified(Boolean verified) {
-        isVerified = verified;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public LocalDate getBirthdate() {
-        return birthdate;
-    }
-
-    public String getVerificationCode() {
-        return verificationCode;
-    }
-
-    public Boolean getVerified() {
-        return isVerified;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false, unique = true)
-    private String email;
-
-    @Column(nullable = false)
-    private String password;
 
     @Column(nullable = false)
     private String firstname;
@@ -104,10 +16,16 @@ public class User {
     @Column(nullable = false)
     private String lastname;
 
+    private LocalDate birthdate;
+
     @Column(nullable = false)
     private String phone;
 
-    private LocalDate birthdate;
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
 
     private String verificationCode;
 
@@ -117,35 +35,127 @@ public class User {
     @JoinColumn(name = "address_id")
     private Address address;
 
+    public User() {
+    }
+
+    public User(Long id, String email, String password, String firstname, String lastname, String phone, LocalDate birthdate, String verificationCode, Boolean isVerified, Address address) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.birthdate = birthdate;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+        this.verificationCode = verificationCode;
+        this.isVerified = isVerified;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getVerificationCode() {
+        return verificationCode;
+    }
+
+    public void setVerificationCode(String verificationCode) {
+        this.verificationCode = verificationCode;
+    }
+
+    public Boolean getVerified() {
+        return isVerified;
+    }
+
+    public void setVerified(Boolean verified) {
+        isVerified = verified;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
                 ", firstname='" + firstname + '\'' +
                 ", lastname='" + lastname + '\'' +
-                ", phone='" + phone + '\'' +
                 ", birthdate=" + birthdate +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
                 ", verificationCode='" + verificationCode + '\'' +
                 ", isVerified=" + isVerified +
                 ", address=" + address +
                 '}';
     }
 
-    public User() {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id != null && id.equals(user.id);
     }
 
-    public User(Long id, String email, String password, String firstname, String lastname, String phone, LocalDate birthdate, String verificationCode, Boolean isVerified, Address address) {
-        this.id = id;
-        this.email = email;
-        this.password = BCrypt.hashpw(password, BCrypt.gensalt());
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.phone = phone;
-        this.birthdate = birthdate;
-        this.verificationCode = verificationCode;
-        this.isVerified = isVerified;
-        this.address = address;
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
