7ba34ec9368f3ca2508075b33320c1d04934287e
Damien <ddubost6998@gmail.com>
2025-07-07 09:50:43 +0200

    Update regex to use more special character

--- src/main/java/fr/humanbooster/electricity_business/dto/UserRegisterDTO.java
+++ src/main/java/fr/humanbooster/electricity_business/dto/UserRegisterDTO.java
@@ -24,7 +24,7 @@ public class UserRegisterDTO {
 
     @NotBlank(message = "Le mot de passe est obligatoire")
     @Size(min = 8, message = "Le mot de passe doit avoir au moins 8 caractères")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!*])(?=\\S+$).{12,}$",
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\w\\s]).{12,}$",
             message = "Le mot de passe doit contenir au moins 12 caractères, un chiffre, une minuscule, une majuscule et un caractère spécial.")
     private String password;
 
