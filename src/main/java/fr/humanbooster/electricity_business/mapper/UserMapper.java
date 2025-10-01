664febbe4250fbfe8b0ef64d48ed10ef3ccba894
Damien <ddubost6998@gmail.com>
2025-10-02 00:15:56 +0200

    Fix error component annotation

--- src/main/java/fr/humanbooster/electricity_business/mapper/UserMapper.java
+++ src/main/java/fr/humanbooster/electricity_business/mapper/UserMapper.java
@@ -5,12 +5,9 @@ import fr.humanbooster.electricity_business.dto.UserRegisterDTO;
 import fr.humanbooster.electricity_business.model.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
-@Mapper()
+@Mapper(componentModel = "spring")
 public interface UserMapper {
-
-    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "verificationCode", ignore = true)
