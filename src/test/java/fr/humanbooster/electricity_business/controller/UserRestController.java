649e8dba866b2518eeca2ca6df91d8b4684f229b
Damien <ddubost6998@gmail.com>
2025-10-02 09:38:03 +0200

    Update annotation TestController in UserRestController

--- src/test/java/fr/humanbooster/electricity_business/controller/UserRestController.java
+++ src/test/java/fr/humanbooster/electricity_business/controller/UserRestController.java
@@ -1,37 +1,46 @@
 package fr.humanbooster.electricity_business.controller;
 
 import fr.humanbooster.electricity_business.service.UserService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserRestController.class)
+@Import(UserRestControllerTest.TestConfig.class)
 class UserRestControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
     private UserService userService;
 
-    @BeforeEach
-    void setUp() {
-        userService = Mockito.mock(UserService.class);
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        public UserService userService() {
+            return mock(UserService.class);
+        }
     }
 
     @Test
     void shouldReturnEmptyList() throws Exception {
-        Mockito.when(userService.getAllUsers()).thenReturn(List.of());
+        // ARRANGE
+        when(userService.getAllUsers()).thenReturn(List.of());
 
         mockMvc.perform(get("/api/users")
                         .contentType(MediaType.APPLICATION_JSON))
